d790cf2a35e8276dc07e913bf7fd20659bf29568
Master Chief <bassamkhanpakistan99@gmail.com>
2024-11-25 00:53:49 +0500

    Added more content changes

--- content/jobs/NRTC/index.md
+++ content/jobs/NRTC/index.md
@@ -1,12 +1,11 @@
 ---
-date: '2016-12-21'
-title: 'Software Engineer Co-op'
-company: 'Starry'
+date: '2024-09-03'
+title: 'Assistant Executive Engineer- Cybersecurity'
+company: 'National Radio Teclecommunication Corporation'
 location: 'Islamabad, Punjab'
-range: 'September - Current'
+range: 'September - Ongoing'
 url: 'https://nrtc.com.pk/index.php'
 ---
 
 - Configured SOC Analyst tools and worked on tools like EDR, NDR, SIEM, SOAR. 
 - Deploed the solutions at NCERT and setup a network and servers for hosting the solutions
-
--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -125,7 +125,7 @@ const About = () => {
     sr.reveal(revealContainer.current, srConfig());
   }, []);
 
-  const skills = ['JavaScript (ES6+)', 'TypeScript', 'React', 'Eleventy', 'Node.js', 'WordPress'];
+  const skills = ['JavaScript (ES6+)', 'SIEM', 'EDR', 'React', 'Python', 'Node.js', 'Markdown'];
 
   return (
     <StyledAboutSection id="about" ref={revealContainer}>
@@ -142,15 +142,14 @@ const About = () => {
 
             <p>
               Fast-forward to today, and I’ve had the privilege of working at{' '}
-              <a href="https://us.mullenlowe.com/">an advertising agency</a>,{' '}
-              <a href="https://starry.com/">a start-up</a>,{' '}
-              <a href="https://www.apple.com/">a huge corporation</a>, and{' '}
-              <a href="https://scout.camd.northeastern.edu/">a student-led design studio</a>. My
-              main focus these days is building accessible, inclusive products and digital
-              experiences at <a href="https://upstatement.com/">Upstatement</a> for a variety of
+              <a href="https://luckyelectricpower.com/">a Power Plant</a>,{' '}
+              <a href="https://www.fauji.org.pk/">a Conglomerate</a>,{' '}
+              <a href="https://mindstormstudios.com/">a Game Development studio</a>, and{' '}
+              <a href="https://luckytextilemills.biz/">a Textile and Apparel manufacturer</a>. My
+              main focus these days is configuring and setting up Defense Solutions at <a href="https://nrtc.com.pk/index.php">National Radio and Telecommunication Coporation</a> for a variety of
               clients.
             </p>
-
+            {/* 
             <p>
               I also recently{' '}
               <a href="https://www.newline.co/courses/build-a-spotify-connected-app">
@@ -159,7 +158,7 @@ const About = () => {
               that covers everything you need to build a web app with the Spotify API using Node
               &amp; React.
             </p>
-
+           */}
             <p>Here are a few technologies I’ve been working with recently:</p>
           </div>
 
