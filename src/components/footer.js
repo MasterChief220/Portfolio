90698754b316a7896fe40a161bb9de6e24bc4c6b
Bassam <bassamkhanpakistan99@gmail.com>
2025-01-01 13:29:35 +0500

    Added Github Api

--- src/components/footer.js
+++ src/components/footer.js
@@ -77,7 +77,7 @@ const Footer = () => {
     if (process.env.NODE_ENV !== 'production') {
       return;
     }
-    fetch('https://api.github.com/repos/bchiang7/v4')
+    fetch('https://api.github.com/repos/MasterChief220/Portfolio')
       .then(response => response.json())
       .then(json => {
         const { stargazers_count, forks_count } = json;
